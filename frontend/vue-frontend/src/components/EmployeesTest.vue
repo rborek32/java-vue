c9adaa44cc9c212d63127187854bc7bbc2046eb1
rborek32 <rborek32@wp.pl>
2023-07-22 15:44:12 +0200

    Frontend init

---
+++ frontend/vue-frontend/src/components/EmployeesTest.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+
+        <h1 class="text-center"> Employees List</h1>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th> Id</th>
+                    <th> First Name</th>
+                    <th> Last</th>
+                    <th> Email</th>
+                </tr>
+
+            </thead>
+            <tbody>
+                <tr v-for="employee in employees" v-bind:key="employee.id">
+                    <td> {{ employee.id }}</td>
+                    <td> {{ employee.firstName }}</td>
+                    <td> {{ employee.lastName }}</td>
+                    <td> {{ employee.email }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import EmployeeService from '../services/EmployeeService';
+
+export default {
+    name: 'EmployeesTest',
+    data() {
+        return {
+            employees: []
+        }
+    },
+    methods: {
+        getEmployees() {
+            EmployeeService.getEmployees().then((response) => {
+                this.employees = response.data;
+            });
+        }
+    },
+    created() {
+        this.getEmployees();
+    }
+}
+</script>
--- springboot-backend/src/main/java/com/javaWithVue/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/javaWithVue/springbootbackend/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("*")
 public class EmployeeController {
 
     @Autowired
